98e7932be673ce05459abe3108e4e84d51c8f5d7
Gabriela Martínez <gabriela.martinez@estudiantes.utec.edu.uy>
2025-03-30 23:19:52 -0300

    Nuevas rutas ffmpeg y ffprobe

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ PROCESSED_DIR = os.path.join(DATA_DIR, "processed")
 TRANSCRIPTIONS_DIR = os.path.join(DATA_DIR, "transcriptions")
 
 # Configuración de FFmpeg
-FFMPEG_PATH = "C:/ProgramData/chocolatey/bin/ffmpeg.exe"
-FFPROBE_PATH = "C:/ProgramData/chocolatey/bin/ffprobe.exe"
+FFMPEG_PATH = "C:\ProgramData\chocolatey\bin\ffmpeg.exe"
+FFPROBE_PATH = "C:\ProgramData\chocolatey\bin\ffprobe.exe"
 
 # Configuración de la aplicación
 APP_TITLE = "Analizador de Velocidad Lectora"
